4164c07573f403840bbe524006bdd959209bd813
cosmograver <cosmograver@DESKTOP-ATAT4UU>
2024-10-31 23:54:45 +0300

    Добавьте файлы проекта.

---
+++ Laba1/Laba1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+string TextToLower(string text) {
+	string alfRusLower = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+	string alfRusUpper = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+	for (int i = 0; i < text.length(); i++) {
+		for (int j = 0; j < alfRusUpper.length(); j++) {
+			if (text[i] == alfRusUpper[j]) {
+				text[i] = alfRusLower[j];
+			}
+		}
+	}
+	return text;
+}
+
+int firstCharToText(string text) {
+
+}
+
+int main() {
+
+}
