28896e91f95c35461d9bba83889372b039f903e4
Aver Alexey <68560245+cosmograve@users.noreply.github.com>
2024-11-01 00:23:57 +0300

    без предварительной обработки ответов

--- Laba1/Laba1.cpp
+++ Laba1/Laba1.cpp
@@ -16,7 +16,7 @@ string TextToLower(string text) {
 	return text;
 }
 
-int firstCharToText(string text) {
+int FirstCharToText(string text) {
 	if (text.length() == 0) {
 		return 0;
 	}
@@ -28,6 +28,70 @@ int firstCharToText(string text) {
 	return 0;
 }
 
-int main() {
+int GetTwoLastNum(int x) {
+	int temp = x;
+	int count = 0;
+	while (x != 0) {
+		x /= 10;
+		count++;
+	}
 
+	if (count != 4) {
+		return -1;
+	}
+
+	int b = x % 10;
+	int a = x / 10 % 10;
+	return a * 10 + b;
+}
+
+int EnterNumber(string text) {
+	int M;
+	while (true) {
+		cout << text << " ";
+		cin >> M;
+		if (cin.fail() == false) {
+			break;
+		}
+		cin.clear();
+		while (cin.get() != '\n');
+	}
+	return M;
+}
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string LastName;
+	cout << "Введите фамилию: ";
+	cin >> LastName;
+
+	string FirstName;
+	cout << "Введите имя: ";
+	cin >> FirstName;
+
+	//перевод в нижний регистр всех букв русских
+	LastName = TextToLower(LastName);
+	FirstName = TextToLower(FirstName);
+
+	// определяем первую букву в тексте и переводим в номер по алфавитному порядку
+	int LN = FirstCharToText(LastName);
+	int FN = FirstCharToText(FirstName);
+
+	int M = EnterNumber("Введите месяц обучения (2 или 9):");
+	int YY = EnterNumber("Введите год обучения (пример: 2024):");
+	int i = EnterNumber("Введите номер лабораторной работы:");
+	int CNT = EnterNumber("Введите кол-во вариантов:");
+
+	YY = GetTwoLastNum(YY);//находим 2 последние цифры в году
+
+	if (LN == 0 || FN == 0 || YY == -1) {
+		cout << "Error!" << endl;
+		return 1;
+	}
+
+	int VAR = (YY + M + LN + LastName.length() + FN + FirstName.length()) *
+		i % CNT + 1;
+	cout << "Ваш номер варианта: " << VAR << endl;
 }
