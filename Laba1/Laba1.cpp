16cb7d28ba6614690bba5d5abf2fbbc269c24c6f
Aver Alexey <68560245+cosmograve@users.noreply.github.com>
2024-11-01 00:11:19 +0300

    update

--- Laba1/Laba1.cpp
+++ Laba1/Laba1.cpp
@@ -2,9 +2,10 @@
 #include <windows.h>
 using namespace std;
 
+const string alfRusLower = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+const string alfRusUpper = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
 string TextToLower(string text) {
-	string alfRusLower = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-	string alfRusUpper = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
 	for (int i = 0; i < text.length(); i++) {
 		for (int j = 0; j < alfRusUpper.length(); j++) {
 			if (text[i] == alfRusUpper[j]) {
@@ -16,7 +17,15 @@ string TextToLower(string text) {
 }
 
 int firstCharToText(string text) {
-
+	if (text.length() == 0) {
+		return 0;
+	}
+	for (int i = 0; i < alfRusLower.length(); i++) {
+		if (text[0] == alfRusLower[i]){
+			return i + 1;
+		}
+	}
+	return 0;
 }
 
 int main() {
